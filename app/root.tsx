fb04b2937d1a0c2a9c2e3cfd3fd841acdb8c9c61
Shifu <gabrielcastillejo25@gmail.com>
2024-11-16 17:15:04 -0500

    chore: add static files

--- app/root.tsx
+++ app/root.tsx
@@ -9,17 +9,8 @@ import type { LinksFunction } from "@remix-run/node";
 
 import style from "./tailwind.css?url";
 
+
 export const links: LinksFunction = () => [
-  { rel: "preconnect", href: "https://fonts.googleapis.com" },
-  {
-    rel: "preconnect",
-    href: "https://fonts.gstatic.com",
-    crossOrigin: "anonymous",
-  },
-  {
-    rel: "stylesheet",
-    href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
-  },
   {
     rel: "stylesheet",
     href : style
@@ -34,6 +25,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <Links />
+        <link rel="icon" href="/favicon.ico" />
       </head>
       <body>
         {children}
@@ -45,5 +37,6 @@ export function Layout({ children }: { children: React.ReactNode }) {
 }
 
 export default function App() {
-  return <Outlet />;
+  return( 
+  <Outlet />);
 }
