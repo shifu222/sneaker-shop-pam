5f792c0d9ef76eccf9be3e3613c4db491d2fc081
Shifu <gabrielcastillejo25@gmail.com>
2024-11-16 16:44:34 -0500

    chore: add interfaces for sneaker entity

---
+++ app/types/sneaker.ts
@@ -0,0 +1,10 @@
+export interface Sneaker {
+    sneaker_id: number,
+    name: string,
+    description: string,
+    image_url: string,
+    price: number,
+    slug: string,
+    gender: string,
+    category: string
+}
