9811933c35209fe29896047f8e1325beb16a6d4f
scptx <134897241+scptx0@users.noreply.github.com>
2024-11-18 11:20:54 -0500

    style: modify offer section style fix(offer section): correct alignment issue

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -13,39 +13,30 @@ export default function Index() {
   return (
     <div>
       <Navbar />
-      <section
-        className="relative bg-[url(/wallsneaker.jpg)] bg-cover bg-center bg-no-repeat"
-      >
-        <div
-          className="absolute inset-0 bg-gray-900/75 sm:bg-transparent sm:from-gray-900/95 sm:to-gray-900/25 ltr:sm:bg-gradient-to-r rtl:sm:bg-gradient-to-l"
-        ></div>
+      <section className="relative bg-[url(/wallsneaker.jpg)] bg-cover bg-center bg-no-repeat w-full h-[500px] flex items-center justify-end">
+        <div className="absolute inset-0 bg-gray-300/10"></div>
 
-        <div
-          className="relative justify-end max-w-screen-xl px-4 py-32 sm:px-6 lg:flex lg:h-screen lg:items-center lg:px-8"
-        >
-          <div className="max-w-xl text-right ltr:sm:text-left rtl:sm:text-right">
-            <h1 className="text-5xl font-extrabold text-rose-500 sm:text-7xl md:text-8xl lg:text-10xl xl:text-[10rem]">
-              40%
-              <strong className="block font-extrabold text-rose-500 text-4xl">
-                off
-              </strong>
+        <div className="relative flex justify-end items-center h-screen px-4 sm:px-6 lg:px-8">
+          <div className="max-w-xl text-right ml-auto">
+            <h1 className="text-5xl text-rose-500 sm:text-7xl md:text-8xl lg:text-9xl xl:text-[8rem]">
+              <strong>40%</strong>
+              <div className="block text-4xl">
+                <strong>off</strong></div>
             </h1>
 
-
-
-
-            <div className="mt-8 flex flex-wrap gap-4 text-center items-start">
+            <div className="mt-8 flex justify-end">
               <Link
                 to={'/sneakers'}
-                className="block w-full rounded bg-rose-600 px-12 py-3 text-sm font-medium text-white shadow hover:bg-rose-700 focus:outline-none focus:ring active:bg-rose-500 sm:w-auto"
+                className="inline-block border-2 border-rose-500 bg-rose-500  rounded px-6 py-2 text-sm font-medium text-white shadow hover:bg-rose-600 hover:border-rose-600 active:bg-rose-400  active:border-rose-400 sm:w-auto"
               >
                 Ver ofertas
               </Link>
-
-
             </div>
           </div>
         </div>
+      </section>
+      <section>
+        catalogo
       </section>
     </div>
 
